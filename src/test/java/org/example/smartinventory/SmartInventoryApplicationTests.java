1dfbb3af7c30685f5f9f533479fe8e9012d116c5
AKing982 <59631931+AKing982@users.noreply.github.com>
2024-08-06 11:33:34 -0600

    initial commit

---
+++ src/test/java/org/example/smartinventory/SmartInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
